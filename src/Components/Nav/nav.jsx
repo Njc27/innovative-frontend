c9ab38bbc825204133b55fbfafea1277493920c6
Njc27 <jc.n@northeastern.edu>
2023-07-19 15:18:52 -0400

    navbar changes

--- src/Components/Nav/nav.jsx
+++ src/Components/Nav/nav.jsx
@@ -9,18 +9,15 @@ const Nav = () => {
    <>
     <div className='navbar'>
     <div className='navbar_container'>
-        <div className='title_logo'>
         <div className='innovate_us'>
             <span className='innovate'><b>innovate</b></span>
             <span className='us'>(us)</span>
             </div>
-        </div>
-        <div className='menu_options'>
-        <nav>
+        
+        <nav className='menu_options'>
             <a href='#'>Workshops</a>
             <a href='#'>About us</a>
         </nav>
-        </div>
         </div>
     </div>
    </>
